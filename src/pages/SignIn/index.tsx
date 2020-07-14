20b4b2377413a923375598ab5b9f9cd49bff3ffa
Cristian Araujo <araujocristian@live.com>
2020-07-13 22:05:14 -0300

    Adicionando imagens do logo

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, Text } from 'react-native';
 
+import logoImg from '../../assets/logo.png';
 import { Container } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
+      <Image source={logoImg} />
       <Text>SignIn</Text>
     </Container>
   );
