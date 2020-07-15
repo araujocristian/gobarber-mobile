48f63d4439f06f9a6f38eb4e259ecc4cc5521c9d
Cristian Araujo <araujocristian@live.com>
2020-07-14 23:03:50 -0300

    Criando components Input e Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Image, Text } from 'react-native';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 import { Container, Title } from './styles';
@@ -8,7 +10,19 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('button');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
